c93a4fe87ffb63e711c728411dfe7a931a35801e
JessicaFerraz13 <jessicaferrazrosario@gmail.com>
2023-02-13 03:08:07 -0300

    mod2

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -3,6 +3,7 @@ package br.com.dio.desafio.dominio;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Dev {
     
@@ -12,13 +13,29 @@ public class Dev {
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
 
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+
     }
 
     public void progredir() {
 
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+
+        }
+        else {
+            System.err.println("Você não está inscrito em nenhum conteúdo.");
+        }
+
     }
 
-    public void calcularTotalXp() {
+    public double calcularTotalXp() {
+
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
 
     }
 
